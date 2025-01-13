515206ec48b9f3b7c1d61ded5bba0f919e9a28f6
StenioEric <stenioeric@gmail.com>
2025-01-13 14:19:16 -0300

    Estruturado com prettier

--- src/app/novarota/header/componentes.js
+++ src/app/novarota/header/componentes.js
@@ -1,8 +1,19 @@
-export function Header({subtopico}) {
+import Link from 'next/link'
+
+export function Header({ subtopico }) {
   return (
-    <header style={{ backgroundColor: "#333", color: "#fff", padding: "10px 0", textAlign: "center" }}>
+    <header
+      style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        padding: "10px 0",
+        textAlign: "center",
+      }}
+    >
       <h1>Carros Incríveis</h1>
       <p>Encontre informações sobre os melhores carros!</p>
+      <Link href="/novarota">Rota1</Link> <br />
+      <br />
       <p>{subtopico}</p>
     </header>
   );
